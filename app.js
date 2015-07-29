6c02a3a7f534373ff6c0c11bf81445ace58a8a29
Steve Fentriss <stevesblog2@gmail.com>
2015-07-28 22:52:29 -0400

    change port from 3000 to 80

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+// sets port 8080 to default or unless otherwise specified in the environment
+app.set('port', process.env.PORT || 80);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
